69e956566da190fec96b5f334f31fd3ffcedbdc3
Jaime <salamanca10@me.com>
2021-12-10 18:38:48 -0500

    page loading ok

--- public/navbar.js
+++ public/navbar.js
@@ -1,6 +1,5 @@
 function NavBar(){
   return(
-
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="#">BadBank</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
